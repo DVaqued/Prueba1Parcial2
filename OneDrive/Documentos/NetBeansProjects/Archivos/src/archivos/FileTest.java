c1ca45f5d21ba2de9fa64f28a4f5734975271198
DVaqued <angeldaniel.vaquedano13@gmail.com>
2024-11-14 14:23:14 -0600

    Finalizado.

---
+++ OneDrive/Documentos/NetBeansProjects/Archivos/src/archivos/FileTest.java
@@ -0,0 +1,102 @@
+package archivos;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FileTest {
+
+    public static void main(String[] args) {
+        MyFile mF = new MyFile();
+        Scanner lea = new Scanner(System.in).useDelimiter("\n");
+        int options = 0;
+
+        do {
+            System.out.println("1- Set Archivo/Folder"
+                    + "\n2- Ver Informacion"
+                    + "\n3- Crear Archivo"
+                    + "\n4- Crear Folder"
+                    + "\n5- Agregar Archivo a Folder"
+                    + "\n6- Borrar"
+                    + "\n7- Comando DIR"
+                    + "\n8- TREE"
+                    + "\n9- Reescribir Archivo"
+                    + "\n10- Añadir a Archivo"
+                    + "\n11- Salir");
+            System.out.print("Escoge una de las opciones: ");
+            try {
+                options = lea.nextInt();
+                switch (options) {
+                    case 1:
+                        System.out.print("Direccion: ");
+                        mF.setFile(lea.next());
+                        break;
+                    case 2:
+                        mF.info();
+                        break;
+                    case 3:
+                        if (mF.crearFile()) {
+                            System.out.println("Se creó el archivo!");
+                        } else {
+                            System.out.println("No se creó el archivo!");
+                        }
+                        break;
+                    case 4:
+                        if (mF.crearFolder()) {
+                            System.out.println("Se creó el folder!");
+                        } else {
+                            System.out.println("No se creó el folder!");
+                        }
+                        break;
+                    case 5:
+                        System.out.print("Nombre de la carpeta a agregar: ");
+                        String folderName = lea.next();
+                        System.out.print("Ruta de destino: ");
+                        String ruta = lea.next();
+                        try {
+                            if (mF.addToFolder(folderName, ruta)) {
+                                System.out.println("Carpeta agregada correctamente");
+                            } else {
+                                System.out.println("No se pudo agregar la carpeta");
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error al agregar la carpeta: " + e.getMessage());
+                        }
+                        break;
+                    case 6:
+                        mF.borrar();
+                        break;
+                    case 7:
+                        mF.dir();
+                        break;
+                    case 8:
+                        mF.tree();
+                        break;
+                    case 9:
+                        System.out.print("Contenido para reescribir el archivo: ");
+                        String contenidoReescribir = lea.next();
+                        mF.reescribirArchivo(contenidoReescribir);
+                        break;
+                    case 10:
+                        System.out.print("Contenido para añadir al archivo: ");
+                        String contenidoAñadir = lea.next();
+                        mF.añadirInfo(contenidoAñadir);
+                        break;
+                    case 11:
+                        System.out.println("Saliendo del sistema.");
+                        break;
+                    default:
+                        System.out.println("Opción no válida");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ingresar una opción válida!");
+                lea.next();
+            } catch (NullPointerException e) {
+                System.out.println("Por favor, seleccionar primero la opción 1");
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (options != 11);
+    }
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Archivos/src/archivos/MyFile.java
@@ -0,0 +1,155 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivos;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ *
+ * @author angel
+ */
+public class MyFile {
+
+    private File myFile = null;
+
+    void setFile(String direccion) {
+        myFile = new File(direccion);
+    }
+
+    void info() {
+        if (myFile.exists()) {
+            System.out.println("\nNombre: " + myFile.getName());
+            System.out.println("Path: " + myFile.getPath());
+            System.out.println("Absoluta: " + myFile.getAbsolutePath());
+            System.out.println("Bytes: " + myFile.length());
+            System.out.println("Modificado en: " + new Date(myFile.lastModified()));
+            System.out.println("Padre: " + myFile.getAbsoluteFile().getName());
+            if (myFile.isFile()) {
+                System.out.println("ES UN ARCHIVO");
+            } else if (myFile.isDirectory()) {
+                System.out.println("ES FOLDER");
+            }
+            System.out.println("-+-+-+-+-+-+-+");
+        } else {
+            System.out.println("No existe!");
+        }
+    }
+
+    boolean crearFile() throws IOException {
+        return myFile.createNewFile();
+    }
+
+    boolean crearFolder() {
+        return myFile.mkdirs();
+    }
+
+    void borrar() {
+        if (antidoto(myFile)) {
+            System.out.println("Borrado!");
+        } else {
+            System.out.println("No se pudo borrar!");
+        }
+    }
+
+    private boolean antidoto(File mf) {
+        if (mf.isDirectory()) {
+            for (File child : mf.listFiles()) {
+                antidoto(child);
+            }
+        }
+        return mf.delete();
+    }
+
+    void dir() {
+        if (myFile.isDirectory()) {
+            System.out.println("Directorio de: " + myFile.getAbsolutePath());
+            System.out.println("");
+            //contadores
+            int cfiles = 0, cdirs = 0, totalBytes = 0;
+            //recorrido
+            for (File child : myFile.listFiles()) {
+                if (!child.isHidden()) {
+                    //ultima modificacion
+                    Date ultimo = new Date(child.lastModified());
+                    System.out.print(ultimo + "\t"); //tabulacion
+
+                    if (child.isDirectory()) {
+                        cdirs++;
+                        System.out.println("<DIR>\t\t");
+                    } else {
+                        cfiles++;
+                        //tbytes = totalizado
+                        totalBytes += child.length();
+                        System.out.println("    \t" + child.length() + "\t");
+                    }
+                    System.out.println(child.getName());
+                }
+            }
+            System.out.println(cfiles + " archivos\t" + totalBytes + " bytes");
+            System.out.println(cdirs + " dirs\t");
+        }
+    }
+
+    private void tree(File dir, String tab) {
+        if (dir.isDirectory()) {
+            System.out.println(tab + dir.getName());
+            for (File child : dir.listFiles()) {
+                if (!child.isHidden()) {
+                    tree(child, tab + "--");
+                }
+            }
+        }
+    }
+
+    void tree() {
+        tree(myFile, "-");
+    }
+
+    boolean addToFolder(String nombreCarpeta, String ruta) throws IOException {
+        File carpeta = new File(ruta);
+        if (myFile.isFile() && myFile.isDirectory()) {
+            File nuevaCarpeta = new File(carpeta, nombreCarpeta);
+
+            return nuevaCarpeta.mkdirs();
+        } else {
+            System.out.println("No se pudo agregar correctamente");
+        }
+        return false;
+    }
+
+    void reescribirArchivo(String contenido) {
+        try (FileWriter fW = new FileWriter(myFile)) {
+            fW.write(contenido + "\n");
+        } catch (IOException e) {
+            System.out.println("No se ha ha podido sobreescribir.");
+        }
+    }
+
+    void añadirInfo(String contenido) {
+        try (FileWriter fW = new FileWriter(myFile, true)) {
+            fW.write(contenido + "\n");
+        } catch (IOException e) {
+            System.out.println("No se ha podido añadir la info.");
+        }
+    }
+
+    void leerArchivo() {
+        if (myFile.exists() && myFile.isFile()) {
+            try (FileReader fR = new FileReader(myFile); BufferedReader bR = new BufferedReader(fR)) {
+                String linea;
+                while ((linea = bR.readLine()) != null) {
+                    System.out.println(linea);
+                }
+            } catch (IOException e) {
+                System.out.println("No se ha podido leer el archivo.");
+            }
+        }
+    }
+}
